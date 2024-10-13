4d8e65c55ed320f050aee912e2c8229da67fc476
Lethargy2718 <youssefsherief590@gmail.com>
2024-10-13 18:45:12 +0200

    Add binary search implementation

---
+++ binarysearch.py
@@ -0,0 +1,35 @@
+import sys
+
+def binary_search(target, data: list):
+    """
+    O(logn)
+    max tries = log2(n) rounding up
+
+    """
+    low  = 0
+    high = len(data) - 1
+    tries = 0
+    while low <= high:
+        mid = (low + high) // 2
+        guess = data[mid]
+        tries += 1
+        if guess == target:
+            print('Found')
+            print(f'Tries: {tries}')
+            return
+        if guess < target:
+            low = mid + 1
+        else: #guess > target
+            high = mid - 1
+    print('Not Found')
+    print(f'Tries: {tries}')
+
+try:
+    target = int(sys.argv[1])
+except (IndexError, ValueError):
+    print("Please provide an integer as a command line argument.")
+    sys.exit(1)
+    
+
+test = [i for i in range(1000)]
+binary_search(int(sys.argv[1]), sorted(test))            
