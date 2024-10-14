5df2fcc2b3e7b0e25750026050b66fa50b583786
Lethargy2718 <youssefsherief590@gmail.com>
2024-10-14 18:23:23 +0200

    ...

--- recursion.py
+++ recursion.py
@@ -1,16 +1,14 @@
-def factorial_iterative(num):
-    dummy = num
-    while True:
-        num *= (dummy - 1)
-        dummy -= 1
-        if dummy == 1:
-            return num
-        
 def factorial_recursive(num):
-    if num == 1:
+    if num <= 1: #In case somebody inputs 0.
         return 1
-    return num * factorial_recursive(num - 1)       
+    return num * factorial_recursive(num - 1)   
 
+def factorial_iterative(num):
+    result = 1
+    for i in range(1, num+1):
+        result = result * i
+    return result    
+    
 def sum(arr):
     """
     recursive sum
@@ -19,9 +17,9 @@ def sum(arr):
         return arr[0]
     return (arr[0] + sum(arr[1:]))
 
-num = 5
+num = 6
 arr = [1,2,4,5]
-print("Using iteration: ", factorial_iterative(num))    
+print("Using iteration: ", factorial_iterative(num))
 print("Using recursion: ", factorial_recursive(num))    
 print('Array: ', arr)
 print('Sum: ', sum(arr))
