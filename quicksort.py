f34f7c90c0e892ca62aac81cd8abbb64a7851943
Lethargy2718 <youssefsherief590@gmail.com>
2024-10-14 18:10:28 +0200

    Updated

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,40 @@
-#hhh
+from random import randint
+
+def quicksort_genexp(arr):
+    """
+    I used a generative expression like the book but picked a random number
+    as the pivot for an average complexity of O(nlogn) instead of O(n^2)
+    obtained when always picking the first number as the pivot.
+    """
+    if len(arr) < 2:
+        return arr
+    i = randint(0, len(arr)-1)     
+    pivot = arr[i]
+    smaller = [j for index, j in enumerate(arr) if i != index and j < pivot]
+    bigger = [j for index, j in enumerate(arr) if i != index and j > pivot]
+    return quicksort_genexp(smaller) + [pivot] + quicksort_genexp(bigger)
+    
+
+def quicksort(arr):
+    """
+    No generative expressions
+    """
+    if len(arr) < 2:
+        return arr
+    i = randint(0, len(arr)-1)
+    pivot = arr[i]
+    smaller = []
+    bigger = []
+    for j in range(len(arr)):
+        if j == i:
+            continue
+        if arr[j] < pivot:
+            smaller.append(arr[j])
+        if arr[j] > pivot:
+            bigger.append(arr[j])
+    return quicksort(smaller) + [pivot] + quicksort(bigger)
+    
+arr = [1,4,6,2,3]
+sorted = quicksort_genexp(arr)
+print(f'Unsorted: {arr}')
+print(f'Sorted: {sorted}')
--- recursion.py
+++ recursion.py
@@ -11,6 +11,17 @@ def factorial_recursive(num):
         return 1
     return num * factorial_recursive(num - 1)       
 
+def sum(arr):
+    """
+    recursive sum
+    """
+    if len(arr) == 1:
+        return arr[0]
+    return (arr[0] + sum(arr[1:]))
+
 num = 5
+arr = [1,2,4,5]
 print("Using iteration: ", factorial_iterative(num))    
 print("Using recursion: ", factorial_recursive(num))    
+print('Array: ', arr)
+print('Sum: ', sum(arr))
